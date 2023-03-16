c47312d70bc133ca2d6c03126b3c11db5445992e
2 2 <2 2@DESKTOP-A82HU2N>
2023-03-16 13:36:19 -0300

    Add project files.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número ímpar: ");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n % 2 == 0)
+            {
+                Console.Write("Número Pár, informe um número ímpar: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+             
+            for (int i = 0; i < n; i += 2)
+            {
+
+                for (int j = n; j > i; j -= 2)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k <= i; k += 1)
+                {
+                    Console.Write("x");
+                    
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = n - 2; i >= 0; i -= 2)
+            {
+
+                for (int j = n + 2; j > i; j -= 2)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = i; k > 0; k -= 1)
+                {
+                    Console.Write("x");
+                    
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
